d7c2c531c2847a4cc8dc529e494893e484c77d47
Daniel MS <164803027+DaniiMS506@users.noreply.github.com>
2024-07-08 12:19:55 -0600

    Revert "Actualización Rama Daniel Meneses"

--- Documentación/Scripts DB/EXC View's.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM tiendaropa.marcasconventas;
-
-SELECT * FROM tiendaropa.prendasconstock;
-
-SELECT * FROM tiendaropa.top5marcasvendidas;
--- Documentación/Scripts DB/Script Select & Filtrado (g.).sql
+++
@@ -1,8 +0,0 @@
--- Consultar todas las prendas y sus detalles
-SELECT * FROM Prenda;
-
--- Filtrar por la fecha '2023-05-20'
-SELECT idPrenda, SUM(Cantidad) AS Cantidad_Vendida
-FROM Venta
-WHERE Fecha = '2024-06-11'
-GROUP BY idPrenda;
